6f698a47a23bdbb246ea03785ed69510f3e75ecd
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-04-27 22:50:16 -0400

    multiple inputs

--- p1.py
+++ p1.py
@@ -1,15 +1,21 @@
 # p1.py
 
 # Other neurons will sent outputs to this neuron as inputs
-inputs = [1.2, 5.1, 2.1]
+inputs = [1, 2, 3, 2.5]
 # inputs will also have uniques weights attached
-weights = [3.1, 2.1, 8.7]
+weights1 = [0.2, 0.8, -0.5, 1.0]
+weights2 = [0.5, -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
+
 # every neuron (this) will have a unique bias
-bias = 3
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
 
-output = 0
 # output = input 1 * bias 1 + input 2 * bias 2 etc...
-for element1, element2 in zip(inputs, weights):
-    output += element1 * element2
-
-print(output+bias)
+outputs = [
+    inputs[0] * weights1[0] + inputs[1] * weights1[1] + inputs[2] * weights1[2] + inputs[3] * weights1[3] + bias1,
+    inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + inputs[3] * weights2[3] + bias2,
+    inputs[0] * weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] + inputs[3] * weights3[3] + bias3
+]
+print(outputs)

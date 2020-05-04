27d186b7f5d89a814402810ccf7a17c7731fc200
Tyler Clark <tylerc.thedeveloper@gmail.com>
2020-05-03 22:50:07 -0400

    putting into a class

--- p1.py
+++ p1.py
@@ -1,21 +1,26 @@
 # p1.py
+import numpy as np
+
+np.random.seed(0)
 
 # Other neurons will sent outputs to this neuron as inputs
-inputs = [1, 2, 3, 2.5]
-# inputs will also have uniques weights attached
-weights1 = [0.2, 0.8, -0.5, 1.0]
-weights2 = [0.5, -0.91, 0.26, -0.5]
-weights3 = [-0.26, -0.27, 0.17, 0.87]
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
 
-# every neuron (this) will have a unique bias
-bias1 = 2
-bias2 = 3
-bias3 = 0.5
 
-# output = input 1 * bias 1 + input 2 * bias 2 etc...
-outputs = [
-    inputs[0] * weights1[0] + inputs[1] * weights1[1] + inputs[2] * weights1[2] + inputs[3] * weights1[3] + bias1,
-    inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + inputs[3] * weights2[3] + bias2,
-    inputs[0] * weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] + inputs[3] * weights3[3] + bias3
-]
-print(outputs)
+class LayerDense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs , self.weights) + self.biases
+
+
+layer1 = LayerDense(4, 5)
+layer2 = LayerDense(5, 2)
+
+layer1.forward(X)
+layer2.forward(layer1.output)
+print(layer2.output)
